98cd357c889d40dc13169c4817e98394e8b81ea4
Phil Daum <philgdaum@gmail.com>
2022-03-29 16:03:16 -0700

    it works but not very well

--- site.js
+++ site.js
@@ -7,3 +7,33 @@ function openNewBackgroundTab(){
     evt.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0,true, false, false, false, 0, null);    
     a.dispatchEvent(evt);
 }
+
+
+
+
+//timed rickroll experience
+//if this 
+//button has been clicked
+//then z-index rickroll =2
+//for (x) time
+
+function chBackcolor(color) {
+    document.body.style.background = color;
+ }
+
+
+
+
+function rickrollHiddenLayer(){    
+    var a = document.createElement("a");    
+    a.href = "https://github.com/PhillipDaum/parallax-practice";    
+    var evt = document.createEvent("MouseEvent");    
+
+    //the tenth parameter of initMouseEvent sets ctrl key    
+    evt.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0,true, false, false, false, 0, null);    
+    a.dispatchEvent(evt);
+}
+
+
+
+
