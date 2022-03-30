39864336956374add5df9cd5e2cd0f428544eff7
Phil Daum <philgdaum@gmail.com>
2022-03-29 17:57:12 -0700

    it works better, but not good enough. java for bacground messed up

--- site.js
+++ site.js
@@ -1,3 +1,4 @@
+//opens one portfolio item in seperate tab
 function openNewBackgroundTab(){    
     var a = document.createElement("a");    
     a.href = "https://github.com/PhillipDaum/parallax-practice";    
@@ -9,29 +10,37 @@ function openNewBackgroundTab(){
 }
 
 
-
-
 //timed rickroll experience
 //if this 
 //button has been clicked
 //then z-index rickroll =2
 //for (x) time
 
-function chBackcolor(color) {
-    document.body.style.background = color;
- }
+
+function RickRollerson(main){
+    document.getElementById("tile-i").className = "bg-transparent";
+}
+
+public class Main {
+    public static void main(String[] args) {
+      for (int i = 0; i < 5; i++) {
+        System.out.println(i);
+      }  
+    }
+  }
+  
 
 
+function RickRollerson(1){
+            document.getElementById("tile-1").className = "bg-transparent";
+}
 
+function RickRollerson(2){
+    document.getElementById("tile-2").className = "bg-transparent";
+}
 
-function rickrollHiddenLayer(){    
-    var a = document.createElement("a");    
-    a.href = "https://github.com/PhillipDaum/parallax-practice";    
-    var evt = document.createEvent("MouseEvent");    
-
-    //the tenth parameter of initMouseEvent sets ctrl key    
-    evt.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0,true, false, false, false, 0, null);    
-    a.dispatchEvent(evt);
+function RickRollerson(3){
+    document.getElementById("tile-3").className = "bg-transparent";
 }
 
 
